44802a0f25081513d7173f80a4463f710b5b3155
Mazem-run <frostybig1@gmail.com>
2025-04-19 10:03:53 +0300

    Update messageModule.js

--- modules/messageModule.js
+++ modules/messageModule.js
@@ -1,6 +1,6 @@
 const {EmbedBuilder, ButtonBuilder, embedLength} = require("discord.js")
 const {needPromotechannel, promotedemoteChannel, trainingsChannel} = require('/config.json')
-const config = require('/config.json')
+const config = require('../config.json')
 
 module.exports = {
    async needPromote(client,username, NewRank, OldRank, Reason) {
